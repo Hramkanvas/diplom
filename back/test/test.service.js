bf8ab364d722846cec26f0fded3a860dd1a501e7
Kamstantsin.Hramyka <Kanstantsin.Hramyka@wolterskluwer.com>
2020-05-21 10:04:13 +0300

    more changes

---
+++ back/test/test.service.js
@@ -0,0 +1,122 @@
+var testcases = [
+    {
+        id: "1",
+        name: "First testcase",
+        projectID: "1",
+        description: 'some description',
+        priority: 'major',
+        label: 'randomLabel',
+        finished: false,
+        assign: "To Me",
+        status: "Blocked"
+    },
+    {
+        id: "2",
+        name: "Second testcase",
+        projectID: "2",
+        description: 'some description',
+        priority: 'minor',
+        label: 'randomLabel2',
+        finished: false,
+        assign: "To Me",
+        status: "Blocked"
+    }
+]
+
+var steps = [
+    {
+        id: "1",
+        testcaseID: "1",
+        description: 'do this',
+        expectedResult: 'should be like this',
+        actualResult: ''
+    },
+    {
+        id: "2",
+        testcaseID: "2",
+        description: 'do this2',
+        expectedResult: 'should be like this2',
+        actualResult: ''
+    }
+]
+
+var lastID = 2;
+var lastStepID = 2;
+
+module.exports = {
+    getAll,
+    createTestcase,
+    delete: _delete,
+    getSteps,
+    updateTestcase
+};
+
+async function getAll(id) {
+    return testcases.filter(testcase => testcase.projectID === id);
+}
+
+async function getSteps(id) {
+    return steps.filter(step => step.testcaseID === id);
+}
+
+async function createTestcase(testcaseParams) {
+    lastID += 1;
+    const newTestcase = {
+        id: lastID.toString(),
+        name: testcaseParams.name,
+        projectID: testcaseParams.projectID,
+        description: testcaseParams.description,
+        priority: testcaseParams.priority,
+        label: testcaseParams.label,
+        finished: false,
+        assign: testcaseParams.assign,
+        status: ""
+    };
+    testcases.push(newTestcase);
+    if (testcaseParams.steps && testcaseParams.steps.length > 0) {
+        testcaseParams.steps.forEach(step => {
+            lastStepID += 1;
+            step.id = lastStepID.toString();
+            step.testcaseID = lastID.toString();
+            steps.push(step);
+        })
+    }
+}
+
+async function updateTestcase(testcaseParams) {
+    const updatedTestcase = {
+        id: testcaseParams.id,
+        name: testcaseParams.name,
+        projectID: testcaseParams.projectID,
+        description: testcaseParams.description,
+        priority: testcaseParams.priority,
+        label: testcaseParams.label,
+        finished: testcaseParams.finished,
+        assign: testcaseParams.assign,
+        status: testcaseParams.status
+    };
+    console.log(testcaseParams);
+    testcases.forEach((element, index) => {
+        if (element.id === testcaseParams.id) {
+            testcases[index] = updatedTestcase;
+        }
+    })
+    steps = steps.filter(step => step.testcaseID !== testcaseParams.id);
+    if (testcaseParams.steps && testcaseParams.steps.length > 0) {
+        testcaseParams.steps.forEach(step => {
+            lastStepID += 1;
+            step.id = lastStepID.toString();
+            step.testcaseID = lastID.toString();
+            steps.push(step);
+        })
+    }
+}
+
+async function _delete(id) {
+    testcases = testcases.filter(testcase => {
+        return testcase.id !== id
+    })
+    steps = steps.filter(step => {
+        return step.testcaseID !== id
+    })
+}
---
+++ back/users/user.service.js
@@ -0,0 +1,71 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const users = [
+    {
+        id: "1",
+        username: "q",
+        hash: "1",
+        firstName: "q",
+        lastName: "q",
+        createdDate: new Date()
+    },
+    {
+        id: "2",
+        username: "a",
+        hash: "2",
+        firstName: "a",
+        lastName: "a",
+        createdDate: new Date()
+    }
+
+]
+
+var lastID = 2;
+
+module.exports = {
+    authenticate,
+    getAll,
+    getById,
+    create,
+    delete: _delete
+};
+
+async function authenticate({ username, password }) {
+    const user = users.filter(user => user.username === username)[0];
+    console.log(password);
+    console.log(user.hash);
+    console.log(user);
+    if (user && password == user.hash) {
+        const token = jwt.sign({ sub: user.hash }, config.secret);
+        return {
+            ...user,
+            token
+        };
+    }
+
+}
+
+async function getAll() {
+    return users;
+}
+
+async function getById(id) {
+    return users.filter(user => user.id === id)[0];
+}
+
+async function create(userParam) {
+    const nn = {};
+    nn.username = userParam.username;
+    nn.firstName = userParam.firstName;
+    nn.lastName = userParam.lastName;
+    nn.createdDate = new Date()
+    lastID += 1;
+    nn.id = lastID.toString();
+    nn.hash = userParam.password;
+    users.push(nn);
+}
+
+async function _delete(id) {
+    // await User.findByIdAndRemove(id);
+    console.log('delete')
+}
